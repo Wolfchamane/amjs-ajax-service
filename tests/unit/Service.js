caee0bbf22b2237978fb2d9d1935d7b080f47d5d
Wolfchamane <arturo.mart.diaz@gmail.com>
2020-09-08 13:26:53 +0200

    [fix] tests/*: replaced swapi url

--- tests/unit/Service.js
+++ tests/unit/Service.js
@@ -100,7 +100,7 @@ AmjsFactory.register('MockModel', MockModel);
 
 (async function ()
 {
-    const host = 'https://swapi.co';
+    const host = 'https://swapi.dev';
     AmjsAjaxAdapterJSON.register(`Adapter::${host}`, AmjsAjaxAdapterJSON);
     const sut = new AmjsAjaxService({
         host,
@@ -114,7 +114,7 @@ AmjsFactory.register('MockModel', MockModel);
 
 (async function ()
 {
-    const host = 'https://swapi.co';
+    const host = 'https://swapi.dev';
     AmjsAjaxAdapterJSON.register(`Adapter::${host}`, AmjsAjaxAdapterJSON);
     const sut = new AmjsAjaxService({
         host,
@@ -124,38 +124,5 @@ AmjsFactory.register('MockModel', MockModel);
         }
     });
     const response = await sut.doRequest();
-    equal(JSON.stringify(response) === JSON.stringify({
-        data : {
-            name         : 'Luke Skywalker',
-            height       : '172',
-            mass         : '77',
-            'hair_color' : 'blond',
-            'skin_color' : 'fair',
-            'eye_color'  : 'blue',
-            'birth_year' : '19BBY',
-            gender       : 'male',
-            homeworld    : 'https://swapi.co/api/planets/1/',
-            films        : [
-                'https://swapi.co/api/films/2/',
-                'https://swapi.co/api/films/6/',
-                'https://swapi.co/api/films/3/',
-                'https://swapi.co/api/films/1/',
-                'https://swapi.co/api/films/7/'
-            ],
-            species : [
-                'https://swapi.co/api/species/1/'
-            ],
-            vehicles : [
-                'https://swapi.co/api/vehicles/14/',
-                'https://swapi.co/api/vehicles/30/'
-            ],
-            starships : [
-                'https://swapi.co/api/starships/12/',
-                'https://swapi.co/api/starships/22/'
-            ],
-            created : '2014-12-09T13:50:51.644000Z',
-            edited  : '2014-12-20T21:17:56.891000Z',
-            url     : 'https://swapi.co/api/people/1/'
-        }
-    }), true, 'doRequest works as expected');
+    equal(response.data.name === 'Luke Skywalker', true, 'doRequest works as expected');
 })();
